13b4aa17f1cd1e61f62b3da927bcdd06624e24ee
rafael45717 <120226885+rafael45717@users.noreply.github.com>
2023-06-06 19:14:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var primeiroNome = "Rafael";
+console.log(primeiroNome);
+
+
+var segundoNome = "Trocoli";
+console.log(segundoNome);
+
+
+var nomeCompleto = primeiroNome.concat(segundoNome);
+console.log(nomeCompleto);
+
+
+
+var a = 10;
+var b = 10;
+var add = a + b;
+console.log(add);
